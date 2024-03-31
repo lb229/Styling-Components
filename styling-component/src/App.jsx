2170db3a85f93dc2a9fa97a0ecd327af68b77856
lb229 <156219119+lb229@users.noreply.github.com>
2024-03-31 19:54:20 +0200

    Add files via upload

---
+++ styling-component/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Welcome } from './components/Welcome'
+
+export function App() {
+
+
+  return (
+    <>
+    <Welcome class="welcome"/>
+    </>
+  )
+}
+
+export default App
---
+++ styling-component/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Welcome(){
+  return (
+    <div className="welcome">
+      <h2>Welcome!</h2>
+      <p>Please do your login.</p>
+    </div>
+  );
+};
